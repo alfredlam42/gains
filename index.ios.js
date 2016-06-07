0c06e88ca0c27e0aed89b6b77529430f60ece3db
alfredlam42 <alfred.lam.42@gmail.com>
2016-06-07 13:26:33 -0700

    Move class to individual file

--- index.ios.js
+++ index.ios.js
@@ -1,120 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
+var React = require("react");
+var ReactNative = require("react-native");
+var {
   AppRegistry,
-  StyleSheet,
+  MapView,
+  View,
   Text,
-  View
-} from 'react-native';
+  StyleSheet
+} = ReactNative;
+var realm = require('./src/class');
 
-const Realm = require('realm');
-
-class intObject{}
-intObject.schema = {
-  name: 'intObject',
-  properties: {
-    value: 'int',
-  }
-};
-
-class User {}
-User.schema = {
-  name:'User',
-  primaryKey: 'id',
-  properties: {
-    id: 'int',
-    name: 'string',
-    password: 'string',
-    age: {type: 'int', optional: true},
-    height: {type: 'int', optional: true},
-    weight: {type: 'int', optional: true},
-    series: {type: 'list', objectType: 'Series'},
-  },
-};
-
-class Series {}
-Series.schema = {
-  name: 'Series',
-  primaryKey: 'id',
-  properties: {
-    id: 'int',
-    name: 'string',
-    categories: {type: 'list', objectType: 'Category'},
-    maxes: {type: 'list', objectType: 'Max'},
-    workouts: {type: 'list', objectType: 'Workout'},
-  },
-};
-
-class Category {}
-Category.schema = {
-  name: 'Category',
-  primaryKey: 'id',
-  properties: {
-    id: 'int',
-    name: 'string',
-  }
-};
-
-class Max {}
-Max.schema = {
-  name: 'Max',
-  primaryKey: 'id',
-  properties: {
-    id: 'int',
-    exercise: 'Exercise',
-    maxWeight: 'int',
-  }
-};
-
-class Exercise {}
-Exercise.schema = {
-  name: 'Exercise',
-  primaryKey: 'id',
-  properties: {
-    id: 'int',
-    name: 'string',
-  }
-};
-
-class Workout {}
-Workout.schema = {
-  name: 'Workout',
-  primaryKey: 'id',
-  properties: {
-    id: 'int',
-    week: 'int',
-    day: 'int',
-    exercises: {type: 'list', objectType: 'Exercise'},
-    set: {type: 'list', objectType: 'intObject'},
-    reps: {type: 'list', objectType: 'intObject'},
-    weight: {type: 'list', objectType: 'intObject'},
-  }
-};
-
-class gains extends Component {
-  render() {
-    let realm = new Realm({
-      schema: [intObject, User, Series, Category, Max, Exercise, Workout],
-      schemaVersion: 1,
-    });
-
+//Component
+var Gains = React.createClass({
+  render: function(){
     realm.write(() => {
       realm.create('User', {id: 1, name: 'Alfred', password: '12345'}, true);
       realm.create('Series', {id: 1, name: 'PH3'}, true);
     })
-
-    let currentUser = realm.objects('User')[0];
-    let series = realm.objects('Series')[0];
-
-    // realm.write(() =>{
-    //   currentUser.series.push(series);
-    // })
-
+    var currentUser = realm.objects('User')[0];
+    var series = realm.objects('Series')[0];
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -130,11 +33,12 @@ class gains extends Component {
           {'\n'}Id: {series.id}
         </Text>
       </View>
-    );
+    )
   }
-}
+})
 
-const styles = StyleSheet.create({
+//Style
+var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -153,4 +57,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('gains', () => gains);
+AppRegistry.registerComponent('gains', ()=>Gains);
---
+++ src/class.js
@@ -0,0 +1,98 @@
+var React = require('react-native');
+var {
+  AppRegistry,
+  MapView,
+  View,
+  Text,
+  StyleSheet
+} = React;
+const Realm = require('realm');
+
+class intObject{}
+intObject.schema = {
+  name: 'intObject',
+  properties: {
+    value: 'int',
+  }
+};
+
+class User {}
+User.schema = {
+  name:'User',
+  primaryKey: 'id',
+  properties: {
+    id: 'int',
+    name: 'string',
+    password: 'string',
+    age: {type: 'int', optional: true},
+    height: {type: 'int', optional: true},
+    weight: {type: 'int', optional: true},
+    series: {type: 'list', objectType: 'Series'},
+  },
+};
+
+class Series {}
+Series.schema = {
+  name: 'Series',
+  primaryKey: 'id',
+  properties: {
+    id: 'int',
+    name: 'string',
+    categories: {type: 'list', objectType: 'Category'},
+    maxes: {type: 'list', objectType: 'Max'},
+    workouts: {type: 'list', objectType: 'Workout'},
+  },
+};
+
+class Category {}
+Category.schema = {
+  name: 'Category',
+  primaryKey: 'id',
+  properties: {
+    id: 'int',
+    name: 'string',
+  }
+};
+
+class Max {}
+Max.schema = {
+  name: 'Max',
+  primaryKey: 'id',
+  properties: {
+    id: 'int',
+    exercise: 'Exercise',
+    maxWeight: 'int',
+  }
+};
+
+class Exercise {}
+Exercise.schema = {
+  name: 'Exercise',
+  primaryKey: 'id',
+  properties: {
+    id: 'int',
+    name: 'string',
+  }
+};
+
+class Workout {}
+Workout.schema = {
+  name: 'Workout',
+  primaryKey: 'id',
+  properties: {
+    id: 'int',
+    week: 'int',
+    day: 'int',
+    exercises: {type: 'list', objectType: 'Exercise'},
+    set: {type: 'list', objectType: 'intObject'},
+    reps: {type: 'list', objectType: 'intObject'},
+    weight: {type: 'list', objectType: 'intObject'},
+  }
+};
+
+var realm = new Realm({
+  schema: [intObject, User, Series, Category, Max, Exercise, Workout],
+  schemaVersion: 1,
+});
+
+module.exports = realm;
