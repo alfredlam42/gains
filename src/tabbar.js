c1150be151eb261992dcbe3a9c63c89fc5af6bc2
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2016-06-08 12:52:22 -0700

    create tabbar functionality

--- src/tabbar.js
+++ src/tabbar.js
@@ -12,19 +12,60 @@ var {
 } = ReactNative;
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      selectedTab: 'home'
+    }
+  },
   render: function() {
     return (
-      <View style={styles.container}>
-      <Text>This is the Tabbar page</Text>
-    </View>
+      <TabBarIOS
+        style={styles.container}
+        tintColor="white"
+        barTintColor="#000080"
+        selectedTab={this.state.selectedTab}>
+        <TabBarIOS.Item
+          selected={this.state.selectedTab === 'home'}
+          title="Home"
+          icon={require('./common/icons/home.png')}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'home',
+            });
+          }}>
+          <Home />
+        </TabBarIOS.Item>
+
+        <TabBarIOS.Item
+          selected={this.state.selectedTab === 'workouts'}
+          title="Workouts"
+          icon={require('./common/icons/workouts.png')}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'workouts',
+            });
+          }}>
+          <Workouts />
+        </TabBarIOS.Item>
+
+        <TabBarIOS.Item
+          selected={this.state.selectedTab === 'logs'}
+          title="Logs"
+          icon={require('./common/icons/logs.png')}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'logs',
+            });
+          }}>
+          <Log />
+        </TabBarIOS.Item>
+      </TabBarIOS>
     )
   }
 });
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    flex: 1
   }
-})
+});
